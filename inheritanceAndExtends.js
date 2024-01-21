6ba33ef33963ee1a8cf4f7e186c4c95c903a1ea2
kareraolivier <kareraolivier@gmail.com>
2024-01-21 16:38:59 +0200

    feat: adding class inheritance

--- inheritanceAndExtends.js
+++ inheritanceAndExtends.js
@@ -1,3 +1,31 @@
 /**
  * Js inheritance
  */
+
+class Person {
+  constructor(_name, _age) {
+    this.name = _name;
+    this.age = _age;
+  }
+
+  fullname() {
+    return `your name is ${this.name} and you are  ${this.age}`;
+  }
+}
+
+class Student extends Person {
+  constructor(_name, _age, _marks) {
+    super(_name, _age);
+    this.marks = _marks;
+  }
+  getMarks() {
+    // return `${this.name} got ${this.marks}`;
+    return `${this.fullname()} and you got ${this.marks}`;
+  }
+}
+
+const pone = new Person("karera", 32);
+console.log(pone.fullname());
+
+const sone = new Student("olivier", 32, 76);
+console.log(sone.getMarks());
